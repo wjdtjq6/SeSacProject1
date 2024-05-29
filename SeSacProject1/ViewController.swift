63eb6cef00df1ffb3715248f1f7214ed33d496c8
jimmy <wjdtjq6@gmail.com>
2024-05-29 18:16:01 +0900

    Initial Commit

---
+++ SeSacProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacProject1
+//
+//  Created by t2023-m0032 on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
